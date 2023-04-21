7566c994439d88c4718184f6e765e1239cf359a8
toran billups <toranb@gmail.com>
2023-04-21 11:38:44 -0500

    added stats implementation

--- mix.exs
+++ mix.exs
@@ -1,13 +1,19 @@
 defmodule Mp3Duration.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+  @description "Decodes MP3s and parses out information"
+
   def project do
     [
       app: :mp3_duration,
-      version: "0.1.0",
+      version: @version,
+      description: @description,
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      docs: docs(),
+      package: package()
     ]
   end
 
@@ -23,6 +29,24 @@ defmodule Mp3Duration.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+    ]
+  end
+
+  defp docs do
+    [
+      main: "Mp3Duration",
+      source_url: "https://github.com/toranb/mp3_duration",
+      source_ref: "v#{@version}",
+      extras: []
+    ]
+  end
+
+  def package do
+    [
+      licenses: ["Apache-2.0"],
+      links: %{
+        "GitHub" => "https://github.com/toranb/mp3_duration"
+      }
     ]
   end
 end
